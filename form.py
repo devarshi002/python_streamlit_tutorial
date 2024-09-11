f0449d9a6182c8b438ffe6d4b8f9f311c6f05765
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2024-09-11 14:12:37 +0530

    updated

--- form.py
+++ form.py
@@ -1,11 +1,20 @@
 import streamlit as st 
-st.markdown("<h1>User Registration</h1>", unsafe_allow_html=True)
-form = st.form("form 1")
-form.text_input("First Name")
-col1,col2 = st.columns(2)
-col1.text_input("first name")
-col2.text_input("last name")
-form.text_input("Email address")
-form.text_input("password")
-form.text_input("confirm password")
-form.form_submit_button("Submit")
+st.markdown("<h1 style = 'text-align: center;'>User Registration</h1>", unsafe_allow_html=True)
+with st.form("form 1",clear_on_submit=True):
+ col1,col2 = st.columns(2)
+ f_name=col1.text_input("first name")
+ l_name=col2.text_input("last name")
+ st.text_input("Email address")
+ st.text_input("password")
+ st.text_input("confirm password")
+ day,month,year =st.columns(3)
+ day.text_input("Day")
+ month.text_input("month")
+ year.text_input("year")
+ s_stat=st.form_submit_button("Submit")
+
+ if s_stat:
+  if f_name == "" and l_name == "":
+   st.warning("Please fill above fields")
+  else:
+   st.success("Submitted successfully")
